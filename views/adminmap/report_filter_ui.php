3679b9b3d81dadbd5e8e0398defe38ca64c6ca50
John Etherton <john.etherton@gmail.com>
2011-10-21 08:52:57 -0600

    added some IDs to the UI elements so they can be further modified by CSS and other things

--- views/adminmap/report_filter_ui.php
+++ views/adminmap/report_filter_ui.php
@@ -1,7 +1,7 @@
-<h3>
+<h3 id="admin_map_and_or_filter_header">
 	<a class="f-title" href="#"><?php echo Kohana::lang('ui_main.category'). " ". Kohana::lang('adminmap.logical_operators'); ?></a>
 </h3>
-<div class="f-simpleGroups-box">
+<div class="f-simpleGroups-box" id="admin_map_and_or_filter_body">
 	<ul class="filter-list fl-logicalOperators">
 		<li>
 			<?php echo Kohana::lang('adminmap.OR') . " "; print form::radio('logical_operator', 'or', $operator=="or","onchange=\"logicalOperatorFilterToggle('or');\" id=\"logicalOperatorRadioOr\"");?>
