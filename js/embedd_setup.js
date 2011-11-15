8efe8a7a05165cd22c64826fa1ca1e31ed0082ca
John Etherton <john.etherton@gmail.com>
2011-11-15 11:12:18 -0700

    when running with the info window plugin this code gets run twice, so I made it so it'll only run once.

--- js/embedd_setup.js
+++ js/embedd_setup.js
@@ -1,7 +1,13 @@
+var adminmap_embed_count = 0;
 $(document).ready(function(){
-	$.get(baseUrl + 'iframemap/setup', 
-			function(data){			
-				$("#map").before(data);				
-			});
+	adminmap_embed_count++;
+	if( adminmap_embed_count == 1)
+	{
+		$.get(baseUrl + 'iframemap/setup', 
+				function(data){			
+					$("#map").before(data);				
+				});
+	}
 	
 });
+
