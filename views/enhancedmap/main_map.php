34274809a9b28b4b66e3fc15385be5c1357bbb65
John Etherton <john.etherton@gmail.com>
2012-06-06 16:42:17 -0600

    now you can use the settings to adjust the size of the map div.

--- views/enhancedmap/main_map.php
+++ views/enhancedmap/main_map.php
@@ -10,7 +10,28 @@
  */
 ?>
 <!-- map -->
-<div class="map" id="map"></div>
+<div class="map" id="map" <?php 
+//setup if we're overriding the width or not
+$width = ORM::factory('enhancedmap_settings')->where('key', 'adminmap_width')->find()->value;
+$height =  ORM::factory('enhancedmap_settings')->where('key', 'adminmap_height')->find()->value;
+
+if(strtolower($width) !=  'other' OR strtolower($height) !=  'other')
+{
+	$retVal = 'style="';
+	if(strtolower($width) !=  'other')
+	{
+		$retVal .= 'width:'.$width.'; ';
+	}
+	if(strtolower($height) !=  'other')
+	{
+		$retVal .= 'height:'.$height.'; ';
+	}
+	
+	$retVal .= '"';
+	echo $retVal;
+} 
+
+?>></div>
 <div id="mapStatus">
 	<div id="mapScale" style="border-right: solid 1px #999"></div>
 	<div id="mapMousePosition" style="min-width: 135px;border-right: solid 1px #999;text-align: center"></div>
